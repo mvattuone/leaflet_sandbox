cf2c1fde0a8b51e76716476e36d43ccdebeae53b
Mike Vattuone <mvattuone@gmail.com>
2014-03-11 02:32:06 -0700

    really rough nav

--- static/js/main.js
+++ static/js/main.js
@@ -1,13 +1,3 @@
-$(document).ready(function() {
-var map = L.map('map').setView([51.505, -0.09], 13);
-
-L.tileLayer('http://{s}.tile.cloudmade.com/5015436ad80f44af82c15054968f1468/997/256/{z}/{x}/{y}.png', {
-    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>',
-    maxZoom: 18
-}).addTo(map);
-
-var marker = L.marker([51.5, -0.09]).addTo(map);
-
 function updateMapContainer() {
 	var width = $(window).width(),
 		height = $(window).height();
@@ -20,9 +10,27 @@ function updateMapContainer() {
 	return map;
 }
 
-$(window).on('resize', function() {
-	updateMapContainer();
-});
+function setEventListeners() {
+	$(window).on('resize', function() {
+		updateMapContainer();
+	});
+}
+
+$(document).ready(function() {
+var map = L.map('map').setView([51.505, -0.09], 13);
+
+L.tileLayer('http://{s}.tile.cloudmade.com/5015436ad80f44af82c15054968f1468/997/256/{z}/{x}/{y}.png', {
+    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>',
+    maxZoom: 18
+}).addTo(map);
+
+var marker = L.marker([51.5, -0.09]).addTo(map);
 
 updateMapContainer();
+
+//Is geocoding on server?  If so, should we just pass 
+//search query to string and parse address/city/etc.
+//on server or do it in client and then send to server?
+//or do we just send lat/long to server?
+
 });
