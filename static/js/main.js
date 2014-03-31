b95cc103fa92e24a7a8c429560c88aca6fe6eb83
Mike Vattuone <mvattuone@gmail.com>
2014-03-31 00:18:27 -0700

    geocoding, added bootstrap for grid system and modal, stripped out internal libs for cdns

--- static/js/main.js
+++ static/js/main.js
@@ -2,35 +2,44 @@ function updateMapContainer() {
 	var width = $(window).width(),
 		height = $(window).height();
 
-	var map = $("#map").css({
-		"width": width,
-		"height":height,
-	})
+	$("#map-container").width(width)
+					   .height(height);
 
-	return map;
+	Map.invalidateSize();			
+}
+
+function showAddress(address) {
+	var g = new google.maps.Geocoder();	
+	g.geocode({ 'address': address }, function(results, status) {
+		latLng = [results[0].geometry.location.k, results[0].geometry.location.A];
+		Map.setView(latLng, 18)
+		L.marker(latLng).addTo(Map);
+	})
 }
 
 function setEventListeners() {
 	$(window).on('resize', function() {
 		updateMapContainer();
 	});
+
+	$("#search").on('submit', function(e) {
+		e.preventDefault(e);
+		address = $(this).find('.search-input').val();
+		showAddress(address);
+	});
 }
 
+Map = L.map('map').setView([51.505, -0.09], 13);
+
 $(document).ready(function() {
-var map = L.map('map').setView([51.505, -0.09], 13);
+
 
 L.tileLayer('http://{s}.tile.cloudmade.com/5015436ad80f44af82c15054968f1468/997/256/{z}/{x}/{y}.png', {
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>',
     maxZoom: 18
-}).addTo(map);
-
-var marker = L.marker([51.5, -0.09]).addTo(map);
+}).addTo(Map);
 
 updateMapContainer();
-
-//Is geocoding on server?  If so, should we just pass 
-//search query to string and parse address/city/etc.
-//on server or do it in client and then send to server?
-//or do we just send lat/long to server?
+setEventListeners();
 
 });
