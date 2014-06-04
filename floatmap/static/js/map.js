76f7bc8e6024c5769d89f065899c8970e0eb93b3
Mike Vattuone <mvattuone@gmail.com>
2014-06-04 00:19:24 -0700

    functional share module for after a user searches for an address

--- floatmap/static/js/map.js
+++ floatmap/static/js/map.js
@@ -14,6 +14,7 @@ function showAddress(address) {
 		latLng = [results[0].geometry.location.k, results[0].geometry.location.A];
 		Map.setView(latLng, 18)
 		L.marker(latLng).addTo(Map);
+		$('#share').removeClass('hidden');
 	})
 }
 
@@ -27,19 +28,41 @@ function setEventListeners() {
 		address = $(this).find('.search-input').val();
 		showAddress(address);
 	});
+
+	$('.fb').on('click', function(e) {
+		if (Object.prototype.toString.call(FB) !== "undefined") {
+			FB.ui({
+			  method: 'share',
+			  href: 'http://www.floatmap.com:8000',
+			}, function(response){
+				console.log('okay')
+			});
+		}
+	});
+
+	$('.twitter').on('click', function(e) {
+		//share window config
+	    var width  = 575,
+	        height = 400,
+	        left   = ($(window).width()  - width)  / 2,
+	        top    = ($(window).height() - height) / 2,
+	        url    = 'https://twitter.com/share?url=http://www.example.com',
+	        opts   = 'status=1' +
+	                 ',width='  + width  +
+	                 ',height=' + height +
+	                 ',top='    + top    +
+	                 ',left='   + left;
+	    
+	    window.open(url, 'twitter', opts);
+ 
+	    return false;
+	 });
 }
 
 Map = L.map('map').setView([44.5000, -89.5], 1);
 
 $(document).ready(function() {
 
-
-// L.tileLayer('http://{s}.tile.cloudmade.com/5015436ad80f44af82c15054968f1468/997/256/{z}/{x}/{y}.png', {
-//     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>',
-//     maxZoom: 18
-// }).addTo(Map);
-
-// L.tileLayer('/static/tiles/{z}/{x}/{y}.png', {maxZoom: 9, opacity:1}).addTo(Map);
 L.tileLayer('/static/floods/{z}/{x}/{y}.png', {maxZoom: 9, opacity:.5}).addTo(Map);
 updateMapContainer();
 setEventListeners();
