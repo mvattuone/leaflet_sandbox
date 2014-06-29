6f7fc5ad05acf4c5bccf4d01bbc2ff4af85d0046
Mike Vattuone <mvattuone@gmail.com>
2014-06-29 14:36:26 -0700

    add fema/noaa sample data

--- floatmap/views.py
+++ floatmap/views.py
@@ -1,6 +1,19 @@
+import os
+import json
 from django.shortcuts import render_to_response
 from django.template import RequestContext
-# Create your views here.
+from django.http import HttpResponse
+
 def map(request):
-	context = {}
-	return render_to_response("map.html",context,context_instance=RequestContext(request))
+    context = {}
+    return render_to_response("map.html",context,context_instance=RequestContext(request))
+
+def floodzones(request):
+    data = open(os.path.abspath('data/floodzones.geojson'))
+    json_data = json.loads(data.read())
+    return HttpResponse(json.dumps(json_data))
+
+def extreme_precipitation(request):
+    data = open(os.path.abspath('data/noaa_ex_precip.geojson'))
+    json_data = json.loads(data.read())
+    return HttpResponse(json.dumps(json_data))
--- urls.py
+++ urls.py
@@ -6,7 +6,8 @@ admin.autodiscover()
 urlpatterns = patterns('',
     # Examples:
     url(r'^$', 'floatmap.views.map', name='map'),
-    url(r'/searchs^$', 'floatmap.views.search', name='search'),
-
+    url(r'^search/$', 'floatmap.views.search', name='search'),
+    url(r'^floodzones/$', 'floatmap.views.floodzones', name='floodzones'),
+    url(r'^extreme_precipitation/$', 'floatmap.views.extreme_precipitation', name='extreme_precipitation'),
     url(r'^admin/', include(admin.site.urls)),
 )
